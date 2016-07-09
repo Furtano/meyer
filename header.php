d307ae73c9c6f3d050a2d71d82f79e0351b55902
C. S <schady@posteo.de>
2016-07-09 09:38:44 +0200

    startseiten fix

--- header.php
+++ header.php
@@ -83,7 +83,11 @@
       </div>
 
       <div class="col-xs-12" >
-
+        <?php if(is_home())
+        {
+          query_posts( 'cat=-3' );
+        };
+        ?>
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
           <?php {the_content();} ?>
         <?php endwhile; endif; ?>
