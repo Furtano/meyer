da78294b29ca96f12fb54140860f30cfc3ca3e19
C. S <schady@posteo.de>
2016-11-06 21:08:18 +0100

    aaa

--- header.php
+++ header.php
@@ -44,51 +44,59 @@
       <div class="wpcntent col-xs-10 col-lg-10">
 
         <div class="col-xs-12" style="padding-left:0px; padding-right:18px;padding-top:0px;">
-          <div id="logologo" class="col-xs-12 col-md-2 col-lg-2">
+          <div id="logologo" class="col-xs-3">
             <a href="http://meyer.webfuchs-entwicklung.de/"><img  src="<?php echo get_template_directory_uri(); ?>/fitdurchessen3.png" style="height: 17vh;"/></a>
           </div>
-          <div class="col-xs-10 hidden-xs hidden-sm " id="bgheader">
+          <div class="col-xs-9 hidden-xs hidden-sm " id="bgheader">
           </div>
         </div>
 
         <!--- HEADER MENU -->
 
-        <div class="col-xs-12 col-lg-12" style="padding-bottom: 15px" >
+        <div class="col-xs-12" style="padding-bottom: 15px" >
 
 
 
 
 
-          <div class="collapse navbar-collapse">
+          <!-- <div class="collapse navbar-collapse"> -->
              <ul class="nav navbar-nav">
                <li class="active"><a href="../index.php">Willkommen</a></li>
-               <li><a href="konzept">Konzept</a></li>
                <li><a href="ueber-uns">Über uns</a></li>
 
+               <!--
                <li class="dropdown ">
                  <a href="einzelberatung" id="drop0">Einzelberatung <b class="caret"></b></a>
                    <ul role="menu" class="dropdown-menu" aria-labelledby="drop1">
                        <li role="presentation"><a href="impedanz" role="menuitem">BIO-Impedanzanalyse</a></li>
                    </ul>
                </li>
+              -->
+              <li class="dropdown "><a href="#" id="drop1" data-toggle="dropdown" class="dropdown-toggle" role="button">Einzelberatung <b class="caret"></b></a>
+                  <ul role="menu" class="dropdown-menu" aria-labelledby="drop1">
 
+              <li><a href="einzelberatung" role="menuitem">Ernährungstherapie</a></li>
+              <li><a href="bia" role="menuitem">BIA-Messung</a></li>
+              </ul>
+             </li>
                <li class="dropdown "><a href="#" id="drop1" data-toggle="dropdown" class="dropdown-toggle" role="button">Kurse <b class="caret"></b></a>
                    <ul role="menu" class="dropdown-menu" aria-labelledby="drop1">
                        <li role="presentation"><a href="kochkurse" role="menuitem">Kochkurse</a></li>
                        <li role="presentation"><a href="ernaehrungskurse" role="menuitem">Ernährungskurse</a></li>
                    </ul>
                </li>
+
                <li><a href="gutscheine">Gutscheine</a></li>
-               <li><a href="onlineberatung">Onlineberatung</a></li>
+               <li><a href="bewegung">Bewegung</a></li>
+               <li><a href="gallerie">Galerie</a></li>
                <li><a href="rezept-des-monats">Rezept des Monats</a></li>
-               <li><a href="gallerie">Gallerie</a></li>
 
                <li><a href="kontakt">Kontakt</a></li>
              </ul>
-           </div><!--/.nav-collapse -->
+          <!-- </div> --><!--/.nav-collapse -->
 
 
-        </div>
+      </div>
 
 
 
